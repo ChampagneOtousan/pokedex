9105023f576a1bec7426f6d972fc6ccc5dccfbaf
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-21 14:20:43 -0400

    added some stuff to JS

---
+++ main.js
@@ -0,0 +1,12 @@
+class Pokemon {
+  constructor(name, id, attack, defense, ability, speed) {
+    this.name = name
+    this.id = id
+    this.attack = attack
+    this.defense = defense
+    this.ability = ability
+    this.speed = speed
+  }
+}
+
+poke1 = new Pokemon()
