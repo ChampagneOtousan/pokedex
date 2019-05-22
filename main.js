dfa3243b5474d305a8de59d99658daa6989659cb
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-22 13:25:02 -0400

    fixed API

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 requestAPI = (name) => {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -6,46 +5,47 @@ requestAPI = (name) => {
             data = JSON.parse(this.responseText)
             id = data.id
             nameof = data.name
-            attack = data.attack
-            speed = data.stat['0'].base_stat
-            special_defense = data.stat['1'].base_stat
-            hp = data.stat['5'].base_stat
-            defense = data.stat['3'].base_stat
+            attack = data.stats[4].base_stat
+            speed = data.stats[0].base_stat
+            special_defense = data.stats[1].base_stat
+            hp = data.stats[5].base_stat
+            defense = data.stats[3].base_stat
+            pokemon = new Pokemon(name, id, attack, speed,special_defense,hp, defense)
+            console.log(pokemon)
+            displayUser()
             }
     };
     xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${name}`, true);
     xhttp.send();
 }
 class Pokemon {
-  constructor(name) {
-    requestAPI(name)
-    this.name = nameof
+  constructor(name, id, attack, speed, special_defense, hp, defense) {
+    this.name = name
     this.id = id
     this.attack = attack
     this.speed = speed
     this.special_defense = special_defense
     this.hp = hp
     this.defense = defense
-
   }
 }
 
 // requestAPI()
-// displayUser = () => {
-//     let cont = document.createElement('div')
-//     let h1 = document.createElement('h1')
-//     let p1 = document.createElement('p')
-//     let img = document.createElement('img')
-//
-//     h1.innerText = user['name']
-//     p1.innerText = user['followers']
-//     img.setAttribute('src', user['image'])
-//     cont.appendChild(h1)
-//     cont.appendChild(p1)
-//     cont.appendChild(img)
-//     document.body.appendChild(cont)
-// }
-//
+displayUser = () => {
+    let cont = document.createElement('div')
+    // let h1 = document.createElement('h1')
+    // let p1 = document.createElement('p')
+    // let img = document.createElement('img')
+
+    cont.innerText = pokemon.id
+    // p1.innerText = user['followers']
+    // img.setAttribute('src', user['image'])
+    // cont.appendChild(h1)
+    // cont.appendChild(p1)
+    // cont.appendChild(img)
+    document.body.appendChild(cont)
+}
+
 // class Pokemon {
 //   constructor(name, id, ability, attack, defense, speed) {
 //     this.name = name
@@ -56,5 +56,5 @@ class Pokemon {
 //     this.speed = speed
 //   }
 // }
-//
+
 // poke1 = new Pokemon()
