caab6c3e572ec51565247060d8c02a0f2d4c4c98
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-29 13:40:57 -0400

    got everything to work, just need to make it look pretty

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
 pokemons = []
 
-requestAPI = (name) => {
+requestAPI = (name, container) => {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.responseText)
+            poke = new Pokemon(
+            nameof = data.name,
+            id = data.id,
+            attack = data.stats[4].base_stat,
+            speed = data.stats[0].base_stat,
+            hp = data.stats[5].base_stat,
+            defense = data.stats[3].base_stat,
+            abilities = data.abilities[0].ability.name,
+            picture = data.sprites.front_default,
+            containerName = container
+          )
 
-            id = data.id
-            nameof = data.name
-            attack = data.stats[4].base_stat
-            speed = data.stats[0].base_stat
-            hp = data.stats[5].base_stat
-            defense = data.stats[3].base_stat
-            abilities = data.abilities[0].ability.name
-
-            pokemon = new Pokemon(name, id, attack, speed,hp, defense)
-            pokemons.push(pokemon)
-            console.log(pokemon)
-            displayUser()
+            //pokemon = new Pokemon(name, id, attack, speed,hp, defense)
+            //pokemons.push(pokemon)
+            //console.log(pokemon)
+            displayUser(poke)
   }
 };
     xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${name}`, true);
     xhttp.send();
 }
-
-pokemon_names = ['zapdos', 'ho-oh', 'articuno']
-for(names = 0; names < pokemon_names.length; names++){
-  requestAPI(pokemon_names[names])
-}
+// 
+// pokemon_names = ['zapdos', 'ho-oh', 'articuno']
+// for(names = 0; names < pokemon_names.length; names++){
+//   requestAPI(pokemon_names[names])
+// }
 
 class Pokemon {
-  constructor(name, id, attack, speed, hp, defense, abilities) {
+  constructor(name, id, attack, speed, hp, defense, abilities, picture, container) {
     this.name = name
     this.id = id
     this.attack = attack
@@ -38,57 +41,42 @@ class Pokemon {
     this.hp = hp
     this.defense = defense
     this.abilities = abilities
+    this.picture = picture
+    container.push(this)
   }
 }
 
-displayUser = () => {
+displayUser = (poki) => {
+  document.getElementById('name').innerHTML = poki.name
+  document.getElementById('id').innerHTML = poki.id
+  document.getElementById('attack').innerHTML = poki.attack
+  document.getElementById('hp').innerHTML = poki.hp
+  document.getElementById('abilities').innerHTML = poki.abilities
 
-    pokemon1 = document.getElementById('poke-1')
-    pokemon2 = document.getElementById('poke-2')
-    pokemon3 = document.getElementById('poke-3')
-    pokemon4 = document.getElementById('poke-4')
-    pokemon5 = document.getElementById('poke-5')
-    pokemon6 = document.getElementById('poke-6')
-    pokemon7 = document.getElementById('poke-7')
 
-    pokemon1.innerText = pokemons[0].name
-    pokemon2.innerText = pokemons[0].id
-    pokemon3.innerText = pokemons[0].attack
-    pokemon4.innerText = pokemons[0].speed
-    pokemon6.innerText = pokemons[0].hp
-    pokemon7.innerText = pokemons[0].abilities
 
-    pokemon1_2 = document.getElementById('poke2-1')
-    pokemon2_2= document.getElementById('poke2-2')
-    pokemon3_2 = document.getElementById('poke2-3')
-    pokemon4_2 = document.getElementById('poke2-4')
-    pokemon5_2 = document.getElementById('poke2-5')
-    pokemon6_2 = document.getElementById('poke2-6')
-    pokemon7_2 = document.getElementById('poke2-7')
 
-    pokemon1_2.innerText = pokemons[1].name
-    pokemon2_2.innerText = pokemons[1].id
-    pokemon3_2.innerText = pokemons[1].attack
-    pokemon4_2.innerText = pokemons[1].speed
-    pokemon6_2.innerText = pokemons[1].hp
-    pokemon7_2.innerText = pokemons[1].abilities
 
-    pokemon1_3 = document.getElementById('poke3-1')
-    pokemon2_3= document.getElementById('poke3-2')
-    pokemon3_3 = document.getElementById('poke3-3')
-    pokemon4_3 = document.getElementById('poke3-4')
-    pokemon5_3 = document.getElementById('poke3-5')
-    pokemon6_3 = document.getElementById('poke3-6')
-    pokemon7_3 = document.getElementById('poke3-7')
-
-    pokemon1_3.innerText = pokemons[2].name
-    pokemon2_3.innerText = pokemons[2].id
-    pokemon3_3.innerText = pokemons[2].attack
-    pokemon4_3.innerText = pokemons[2].speed
-    pokemon6_3.innerText = pokemons[2].hp
-    pokemon7_3.innerText = pokemons[2].abilities
-}
-
-function showPokemon() {
+  img = document.getElementById('picture')
+  img.setAttribute("src", poki.picture)
 
 }
+
+function find() {
+  let x = document.getElementById("pokesearch")
+  esteban.get(x.value)
+}
+
+class Trainer {
+  constructor(name) {
+    this.name = name
+    this.container = []
+  }
+  get(name) {
+    requestAPI(name, this.container)
+  }
+  all() {
+    console.log(this.container)
+  }
+}
+esteban = new Trainer("estebanito")
